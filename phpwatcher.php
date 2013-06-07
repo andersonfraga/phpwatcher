ef74bc5fbf7c9f60860d3432e41690b42cc784d7
Anderson Fraga <contato@andersonfraga.net>
2013-06-07 11:35:32 -0300

    Corrigido busca pelos arquivos

--- phpwatcher.php
+++ phpwatcher.php
@@ -1,64 +1,77 @@
 <?php
 
-function phpwatcher($glob, \Closure $func) 
+function phpwatcher($path, $pattern, \Closure $func)
 {
-	$watch = new Phpwatcher($glob);
+    $watch = new Phpwatcher($path, $pattern);
 
-	while(1) {
-		if($updated = $watch->isUpdated()) {
-			$func($updated);
-		}
+    $watch->isUpdated();
+    echo "{$watch->getNumFiles()} files in standby\n";
 
-		sleep(1);
-	}
+    while(1) {
+        if($updated = $watch->isUpdated()) {
+            $func($updated);
+        }
+
+        sleep(1);
+    }
 }
 
-class Phpwatcher 
+class Phpwatcher
 {
-	private $_pattern;
-	private $_cache = array();
+    private $_pattern, $_path;
+    private $_cache = array();
 
-	public function __construct($glob)
-	{
-		$this->_pattern = $glob;
-	}
+    public function __construct($path, $pattern)
+    {
+        $this->_path    = $path;
+        $this->_pattern = $pattern;
+    }
 
-	public function isUpdated()
-	{
-		if(empty($this->_cache)) {
-			$this->_populate();
-		}
+    public function isUpdated()
+    {
+        if(empty($this->_cache)) {
+            $this->_populate();
+        }
 
-		foreach($this->_cache as $file => $mtime) {
-			$new_mtime = filemtime($file);
+        foreach($this->_cache as $file => $mtime) {
+            $new_mtime = filemtime($file);
 
-			if($new_mtime != $mtime) {
-				$this->setCache($file, $new_mtime);
-				$this->sortCache();
+            if($new_mtime != $mtime) {
+                $this->setCache($file, $new_mtime);
+                $this->sortCache();
 
-				return $file;
-			}
-		}
+                return $file;
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	private function _populate()
-	{
-		foreach (new GlobIterator($this->_pattern, FilesystemIterator::SKIP_DOTS) as $file) {
-			$this->setCache($file->getPathname(), $file->getMTime());
-		}
+    public function getNumFiles()
+    {
+        return count($this->_cache);
+    }
 
-		$this->sortCache();
-	}
+    private function _populate()
+    {
+        $ite    = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($this->_path));
+        $files  = new RegexIterator($ite, '/' . $this->_pattern . '/', RecursiveRegexIterator::GET_MATCH);
 
-	private function setCache($file, $mtime)
-	{
-		$this->_cache[$file] = $mtime;
-	}
+        foreach($files as $file) {
+            $file = $file[0];
+            $this->setCache($file, filemtime($file));
+        }
 
-	private function sortCache()
-	{
-		asort($this->_cache);
-	}
+        $this->sortCache();
+    }
+
+    private function setCache($file, $mtime)
+    {
+        $this->_cache[$file] = $mtime;
+    }
+
+    private function sortCache()
+    {
+        asort($this->_cache);
+    }
 };
